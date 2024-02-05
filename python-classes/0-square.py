0b5b3ab524fbcf521dfae9af6664a7c83e20d53d
ThomasVrd11 <Thomasviard@orange.fr>
2024-02-05 09:43:22 +0100

    0 and 1

---
+++ python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a square."""
+
+
+class Square:
+    """
+        Empty class that defines a square.
+    """
+    pass
---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a square."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size):
+        self.__size = size
