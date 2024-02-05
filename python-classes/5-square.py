497e8c54782e8aa24752a1cc47fbc6d9497ad9c6
ThomasVrd11 <Thomasviard@orange.fr>
2024-02-05 10:10:11 +0100

    5 better comments again

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -6,15 +6,36 @@ class Square:
     """Represents a square"""
 
     def __init__(self, size=0):
+        """Initialize a new Square instance.
+
+        Args:
+            size (int): The size of the square.
+
+        """
         self.__size = size
 
     @property
     def size(self):
+        """Get the size of the square.
+
+        Returns:
+            int: The size of the square.
+
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Set the size of the square."""
+        """Set the size of the square.
+
+        Args:
+            value (int): The new size of the square.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than 0.
+
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -22,5 +43,10 @@ class Square:
         self.__size = value
 
     def area(self):
-        """Calculate the area of the square."""
+        """Calculate the area of the square.
+
+        Returns:
+            int: The area of the square.
+
+        """
         return self.__size ** 2
