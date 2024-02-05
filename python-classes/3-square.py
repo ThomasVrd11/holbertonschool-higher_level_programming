08a1f077ca3ca03beb4e323030f19f08e60909e4
ThomasVrd11 <Thomasviard@orange.fr>
2024-02-05 09:52:32 +0100

    3

---
+++ python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines a square."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Returns the current square area."""
+        return self.__size ** 2
