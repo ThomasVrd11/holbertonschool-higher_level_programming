ea0e3400647af5505e8d3e24f33a7df35e55925a
ThomasVrd11 <Thomasviard@orange.fr>
2024-02-05 11:19:00 +0100

    Add square class definition and constructor

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,4 +1,8 @@
+#!/usr/bin/python3
+"""Defines a square."""
+
 class Square:
+    """Represents a square."""
     def __init__(self, size=0, position=(0, 0)):
         """
         Initializes a Square object.
