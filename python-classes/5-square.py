237dbe55d10bc31e29c7aa7e446d029fc01a4963
ThomasVrd11 <Thomasviard@orange.fr>
2024-02-05 10:06:23 +0100

    5

---
+++ python-classes/5-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Defines a square."""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
