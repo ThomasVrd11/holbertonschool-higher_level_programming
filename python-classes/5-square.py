7aef34c011f176ac82c2d14cc4ce7b9b04992c57
ThomasVrd11 <Thomasviard@orange.fr>
2024-02-05 11:13:08 +0100

    5 please

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,25 +1,39 @@
+#!/usr/bin/python3
+"""define square class"""
+
+
 class Square:
+    """Square"""
+
     def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
+
+    def area(self):
+        """Area of Square"""
+        return self.__size * self. __size
 
     @property
     def size(self):
+        """Size"""
         return self.__size
 
     @size.setter
     def size(self, value):
-        if not isinstance(value, int):
+        """size"""
+        if type(value) is not int:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-    def area(self):
-        return self.__size ** 2
-
     def my_print(self):
-        if self.__size == 0:
+        for i in range(self.__size):
+            for y in range(self.__size):
+                print("#", end='')
             print()
-        else:
-            for _ in range(self.__size):
-                print("#" * self.__size)
+        if self.__size == 0:
+            print("")
