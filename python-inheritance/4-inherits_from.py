69a974eb2b202b6fd5c7c86b61539a69cb4dbe29
ThomasVrd11 <Thomasviard@orange.fr>
2024-02-14 15:39:46 +0100

    bon

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,4 +5,4 @@
 def inherits_from(obj, a_class):
     """inherits_from"""
 
-    return issubclass(type(obj), a_class) and not isinstance(obj, a_class)
+    return isinstance(obj, a_class) and type(obj) is not a_class
