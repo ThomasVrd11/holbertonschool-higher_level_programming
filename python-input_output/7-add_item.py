466b77e92650d27f4ea41f0f7394ebf02d43a7b5
ThomasVrd11 <Thomasviard@orange.fr>
2024-02-14 14:29:33 +0100

    enfin?

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,9 +7,9 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 filename = "add_item.json"
 
-try: 
+try:
     my_list = load_from_json_file(filename)
-except:
+except Exception:
     my_list = []
 
 for argument in argv[1:]:
