fdb641762b961a4749786276f67ab828e3aeab5b
ThomasVrd11 <Thomasviard@orange.fr>
2024-02-14 13:56:30 +0100

    seven fix

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-"""define function add_item"""
+""" import all module """
 import sys
+
 
 save = __import__('5-save_to_json_file').save_to_json_file
 load = __import__('6-load_from_json_file').load_from_json_file
 
 
-def add_item(args):
-    """function that adds all arguments to a Python list"""
+def lists(argument):
+    """ function that adds all arguments to a Python list, and then save them to a file"""
     try:
-        my_list = load(add_item.json)
-    except BaseException:
-        my_list = []
+        value = load("add_item.json")
+    except FileNotFoundError:
+        value = []
 
-    my_list += args
-    save(my_list, add_item.json)
+    value += argument
+    save(value, "add_item.json")
 
 
-args = sys.argv[1:]
-add_item(args)
+argument = sys.argv[1:]
+lists(argument)
