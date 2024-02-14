be1bd1c1da17bb79f49d3ab9920d905d32ad8538
ThomasVrd11 <Thomasviard@orange.fr>
2024-02-14 14:01:45 +0100

    clearer

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -7,12 +7,12 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    triangles = [[1]]
-    while len(triangles) != n:
-        tri = triangles[-1]
+    pascalT = [[1]]
+    while len(pascalT) != n:
+        tri = pascalT[-1]
         tmp = [1]
         for i in range(len(tri) - 1):
             tmp.append(tri[i] + tri[i + 1])
         tmp.append(1)
-        triangles.append(tmp)
-    return triangles
+        pascalT.append(tmp)
+    return pascalT
