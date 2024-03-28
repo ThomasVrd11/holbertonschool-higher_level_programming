8c3101ff890a452d356ac5d5c47e825594c46470
ThomasVrd11 <Thomasviard@orange.fr>
2024-03-28 14:38:58 +0100

    Add SQL queries for tasks 3 to 7

---
+++ SQL_more_queries/3-force_name.sql
@@ -0,0 +1,5 @@
+-- tASK : 3
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- TASK : 7
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+    id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
+);
